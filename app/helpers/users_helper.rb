f2c2f7d82909614c1982b9dc15d9f2f2a75a1279
Edward Zeng <edward.zeng.2010@gmail.com>
2013-07-17 17:39:40 +0800

    Add custom gravatar size to gravatar_for helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,15 @@
 module UsersHelper
   # Returns the Gravatar for the given user.
-  def gravatar_for(user)
+  # Basic image request URL looks like this:
+  # http://www.gravatar.com/avatar/HASH
+  # By default, images are presented at 80*80, cusomed by s= or size=
+  # http://www.gravatar.com/avatar/HASH?s=40
+  # Secure requests served over SSL (HTTPS)
+  # https://secure.gravatar.com/avatar/HASH
+  def gravatar_for(user, options = { size: 50 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "http://www.gravatar.com/avatar/#{gravatar_id}.png"
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
